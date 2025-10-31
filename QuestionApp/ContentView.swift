1e7c008b350b95f5dcb0c69d84631a238c9600df
Mrferny123 <nissiann@icloud.com>
2025-10-30 20:17:24 -0400

    Worked on home page ui

--- QuestionApp/ContentView.swift
+++ QuestionApp/ContentView.swift
@@ -8,25 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State private var message = ""
     var body: some View {
-        NavigationStack {
+    NavigationStack {
+        ZStack {
+            LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+            Color(red: 124.0/255.0, green: 127.0/255.0, blue: 101.0/255.0)
+                .ignoresSafeArea(.all)
                 
                 VStack {
-//                    Spacer()
-//                        .frame(height: 20)
-                    Text("Welcome to a quiz to test your Bible knowledge")
+                    Text("Do you really know your Bible?")
                         .font(.largeTitle)
-//                        .padding(.top, 50.0)
+                        Spacer()
+                            .frame(height: 20)
+                    NavigationLink(destination: Question1()) {
+                        Text("Start")
+                            .foregroundStyle(Color.white)
+                    }
                 }
                 
                 .padding()
-//                Spacer()
-                NavigationLink(destination: Question1()) {
-                    Text("Start")
-
-                }
+                //                Spacer()
+            }
         }
     }
 }
